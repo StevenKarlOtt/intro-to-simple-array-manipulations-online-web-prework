cb96523cbbe3372c4df68cc06d957b0e91c80118
Steven Ott <stevenkott@icloud.com>
2018-11-13 23:49:11 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,26 @@
+def using_push(array, yes)
+  array.push(yes)
+end
+
+def using_unshift(array, yes)
+  array.unshift(yes)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_wityh_args(array)
+  array.shift(2)
+end
+
+def using_concat(array, array)
+  
